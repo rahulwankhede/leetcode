b11c157f33cc40e46b839b29f1a53c978ae1bc4a
Rahul Wankhede <rwankhede042@gmail.com>
2019-02-22 22:10:55 +0530

    made solution more pythonic

--- medium/maxIncreaseKeepingSkyline.py
+++ medium/maxIncreaseKeepingSkyline.py
@@ -1,13 +1,13 @@
-class Solution:
-    def maxIncreaseKeepingSkyline(self, grid: 'List[List[int]]') -> 'int':
-        n = len(grid)
-        top = [0]*n
-        left = [0]*n
-        for i in range(n):
-            top[i] = max(grid[j][i] for j in range(n))
-            left[i] = max(grid[i][j] for j in range(n))
-        count = 0
-        for i in range(n):
-            for j in range(n):
-                count += min(top[i], left[j]) - grid[i][j]
-        return count
+def maxIncreaseKeepingSkyline(grid: 'List[List[int]]') -> 'int':
+    top = [max(row) for row in grid]
+    left = [max(col) for col in zip(*grid)]
+        
+    return sum((min(top[c], left[r]) - val)
+            for r, row in enumerate(grid)
+            for c, val in enumerate(row)
+            )
+
+"""testing"""
+
+grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+print(maxIncreaseKeepingSkyline(grid))
